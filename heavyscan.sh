25a52cc9438623530a6957c08f05c2b56201862e
Jay <jaybunn.jobs@gmail.com>
2024-10-22 13:20:45 +1100

    Add files via upload

---
+++ heavyscan.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+read -r -p "ENTER IP=" IP
+
+# DEFINE PORTS HERE------------
+
+ports="-p 0-25,80,111,135,443,445,554,1720-1730,3389,8080"
+
+# Ensure the IP is provided
+if [ -z "$IP" ]; then
+  echo "IP address cannot be empty."
+  exit 1
+fi
+
+
+read -r -p "OUTPUT FILENAME=" OUT
+
+# Check if output provided
+if [ -z "$OUT" ]; then
+    sudo nmap $ports -sC -sV "$IP"
+    exit 1
+fi
+
+sudo sudo nmap $ports -sC -sV -oN "$OUT" "$IP"
---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if sudo apt install nmap ;
+
+then
+
+    echo "|-- heavyscan/nmap is installed and ready --|"
+
+else
+
+    echo "|-- heavyscan/nmap not installed, refer to error message --|"
+
+fi
