5dc4a834c1a027f17ff4cd3eebc190fbee83b5ec
Jay <jaybunn.jobs@gmail.com>
2024-10-24 19:17:42 +1100

    Update heavyscan.sh
    added port 3306

--- heavyscan.sh
+++ heavyscan.sh
@@ -4,7 +4,7 @@ read -r -p "ENTER IP=" IP
 
 # DEFINE PORTS HERE------------
 
-ports="-p 0-25,80,111,135,443,445,554,1720-1730,3389,6379,8080"
+ports="-p 0-25,80,111,135,443,445,554,1720-1730,3306,3389,6379,8080"
 
 # Ensure the IP is provided
 if [ -z "$IP" ]; then
